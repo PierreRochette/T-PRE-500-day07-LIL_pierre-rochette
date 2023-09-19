a1f43239e49ba5c827918c323751054861b0e3f2
Pierre Rochette <pierre.rochette@epitech.eu>
2023-09-19 14:56:53 +0200

    Pendu de base terminé. Il faut modifier le code pour permettre d'écrire plusieurs lettres.

--- hangman.py
+++ hangman.py
@@ -3,21 +3,49 @@ import random
 words_list = ['HELLO', 'GOODBYE', 'WORLD', 'MARS', 'EARTH', 'PLEASE']
 word = words_list[random.randint(0, len(words_list) - 1)]
 blank_word = list(word)
-print(blank_word)
 player_count = len(blank_word)
 for i in range(len(blank_word)) :
     blank_word[i] = "_"
+print(blank_word)
+
 
 # Tout fait l'effet attendu jusqu'ici. 
 
-while player_count < 0 : 
+while player_count > 0 : 
 
     while "_" in blank_word :
 
-        user_guess = str(input("Entrez une lettre : "))
+        user_guess = str(input("Entrez une lettre : ")).upper()
         if len(user_guess) > 1 :
-            print("Veuillez n'entrer qu'une seule lettre à la fois. ")
 
+            #CETTE PARTIE DU CODE VA ÊTRE MODIFIEE POUR PERMETTRE DE METTRE PLUSIEURS LETTRES EN MÊME TEMPS
+
+            print("Veuillez n'entrer qu'une seule lettre à la fois.")
+            continue
+
+        found = False
+
+        for j in range(len(word)) :
+            if word[j] == user_guess and blank_word[j] == "_" :
+                blank_word[j] = user_guess
+                found = True 
+
+        if not found :
+            print("Lettre incorrecte. Vous perdez un point. Réessayez.")
+            player_count -= 1
+            print("Nombre de tentatives restantes : ", player_count)
+            if player_count == 0 : 
+                print("Vous avez perdu.")
+                break
+
+        print(blank_word)
+    
+
+    if "_" not in blank_word :
+        print("Vous avez gagné champion ! Le mot était : ", "".join(blank_word))
+        break
+
+print("Fin du jeu")
 
 
 
