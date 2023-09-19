162dfa88a0de593a57676e946df2cbafb26bf09d
Pierre Rochette <pierre.rochette@epitech.eu>
2023-09-19 10:22:00 +0200

    Modification en cours

--- hangman.py
+++ hangman.py
@@ -1,2 +1,12 @@
+import random
+
 player_count = 0
 words_list = ["WAREHOUSE", "SHARINGAN", "LILLE", "PARIS", "ROUBAIX"]
+
+def select_random_word(words_list) :
+    return words_list[random.randint(0, (len(words_list) - 1))]
+
+word_to_guess = select_random_word(words_list)
+print(word_to_guess)
+
+blanked_word = ''
